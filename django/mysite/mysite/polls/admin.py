37f31e77ec4116b8b259a92f93567335af154a7d
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-02 11:11:27 -0300

    commit-inicial

---
+++ django/mysite/mysite/polls/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
