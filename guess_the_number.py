2b35be134aca401fe5ada956349e912d8c006a5b
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-09 23:27:35 -0300

    perfeito-funcionamento-basico

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,18 +1,28 @@
-import random
+from random import Random, randint, choice
 
 class GessNumberSimulator():
     def __init__(self):
         self.nameprogram = print('\033[1;34mGess Number Simulator\033[m')
 
-    def Start(self):
-        numbers = [7,8,10,11,55,23,65,85,91,0,12,78]
-        chosen = random.choice(numbers)
-        numberoftimes = 1
-        while numberoftimes < 3:
+    def Start():
+        chosen = randint(1,60)
+        numberofchances = 10
+        print(f'')
+        while numberofchances > 0:
             userchoice = int(input('Insert your gess > '))
-            if choice == chosen:
+            if userchoice == chosen:
                 print('Congratulations! You ´ re right! ')
-            elif choice > chosen:
+                break
+            elif userchoice < chosen:
                 print('Almost there! It´s a bigger number!')
+                numberofchances -= 1
+                if numberofchances != 0:
+                    print(f'You can try more {numberofchances} time(s)')
+            elif userchoice > chosen:
+                print('A little less!')
+                numberofchances -= 1
+                if numberofchances != 0:
+                    print(f'You can try more {numberofchances} time(s)')
+        print(f'What a shame, your chances is over! The number chosen is {chosen} Try again and good luck in the next time!')
 
-
+Guess1 = GessNumberSimulator.Start()
