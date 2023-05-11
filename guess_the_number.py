f1883c5f3748faa1bed308411c01e24f53323e59
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-11 09:59:48 -0300

    readaptando-o-codigo-a-minha-personalidade

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,32 +1,46 @@
-from random import Random, randint, choice
+import random
+import PySimpleGUI as sg
 
 class GessNumberSimulator():
     def __init__(self):
-        self.nameprogram = print('\033[1;34mGess Number Simulator\033[m')
+        self.randon_value = 0
+        self.min_value = 1
+        self.max_value = 60
+        self.number_chances = 10
 
-    def Start():
-        chosen = randint(1,60)
-        numberofchances = 10
-        print(f'')
+        layout = [
+            [sg.Text('Guess Number Simulator')],
+            [sg.Input())]
+        ]
+
+    def Start(self):
+        self.GenerateNumber()
+        self.RequestNumber()
         try:
-            while numberofchances > 0:
-                userchoice = int(input('Insert your gess > '))
-                if userchoice == chosen:
-                    print('Congratulations! You ´ re right! ')
+            while self.number_chances != 0:
+                if int(self.quick_value) > self.randon_value:
+                    print(f'\033[1;32mA little less! (Number of chances: {self.number_chances})\033[m')
+                    self.number_chances -= 1
+                    self.RequestNumber()
+                elif int(self.quick_value) < self.randon_value:
+                    print(f'\033[1;31mA little more! (Number of chances: {self.number_chances})\033[m')
+                    self.number_chances -= 1
+                    self.RequestNumber()
+                if int(self.quick_value) == self.randon_value:
+                    self.try_again = False
+                    print(f'\033[1;34m🎉🎉🎉 CONGRATULATIONS! The number chosen is {self.randon_value}! You ´ re right! 🎉🎉🎉\033[m')
                     break
-                elif userchoice < chosen:
-                    print('Almost there! It´s a bigger number!')
-                    numberofchances -= 1
-                    if numberofchances != 0:
-                        print(f'You can try more {numberofchances} time(s)')
-                elif userchoice > chosen:
-                    print('A little less!')
-                    numberofchances -= 1
-                    if numberofchances != 0:
-                        print(f'You can try more {numberofchances} time(s)')
-            print(f'What a shame, your chances is over! The number chosen is {chosen} Try again and good luck in the next time!')
+                if self.number_chances == 0:
+                    print(f'\033[1;35m😢😢😢 What a shame! Your chances is over! This number chosen was {self.randon_value}! More luck in the next time!\033[m')
         except:
-            print('Please, incert only integers numbers!')
+            print('\033[1;031mPlease insert only integers numbers!\033[m')
 
-GessNumberSimulator()
-Guess1 = GessNumberSimulator.Start()
+    def RequestNumber(self):
+        self.quick_value = input('Guess any number: ')
+    
+    def GenerateNumber(self):
+        self.randon_value = random.randint(self.min_value, self.max_value)
+        
+
+Guess1 = GessNumberSimulator()
+Guess1.Start()
