7171566ae97f44b3bacd16e1269bed0387789259
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-18 10:38:29 -0300

    inserindo-GUI-ao-projeto-Status:-sem-sucesso

--- decide_for_me.py
+++ decide_for_me.py
@@ -1,4 +1,5 @@
 from random import choice
+import PySimpleGUI as sg
 
 class DecideForMe:
     def __init__(self):
@@ -8,10 +9,31 @@ class DecideForMe:
             'I don´t know answer this question.',
             'I´m not in the mood to talk today.'
         ]
-    
     def Start(self):
-        input('What this your question? > ')
-        print(choice(self.answers))
+                                    
+        layout = [
+            [sg.Text('Your question:')],
+            [sg.Input(size=(18,0), key='QuestionValue')],
+            [sg.Button('Ask!')],
+            [sg.Output(size=(20,10))]
+        ]
+                                    
+        self.window = sg.Window('Decide for Me', layout=layout)
+
+        try:
+            while True:
+                self.event, self.values = self.window.Read()
+                self.ReceiveQuestion()
+                if self.event == 'Ask!':
+                    print(choice(self.answers))
+        except:
+             print('Please, enter a valid question.')
+             self.Start()
+             
+    def ReceiveQuestion(self):
+         input('What this your question? > ')
+         
+
 
 question = DecideForMe()
 question.Start()
