eb79ff94edf118a533fc217e1a1517ddaab75e77
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-19 10:36:54 -0300

    código para entender o básico do PySimpleGUI

--- Receive_Codes.py
+++ Receive_Codes.py
@@ -1,48 +1,25 @@
-import random 
 import PySimpleGUI as sg
 
-class GuessTheNumber:
-    def __init__(self):
-        self.random_value = 0
-        self.mininum_value = 1
-        self.maximum_value = 100
-        self.try_again = True
-    
-    def Start(self):
-        layout = [
-            [sg.Text('Your guess',size=(39,0))],
-            [sg.Input(size=(18,0),key='GuessValue')],
-            [sg.Button('Guess!')],
-            [sg.Output(size=(39,10))]
-        ]
-        
-        self.window = sg.Window('Guess the number!',layout=layout)
-        self.GenerateRandomNumber()
-        try:
-            while True:
-                self.event, self.values = self.window.Read()
-                if self.event == 'Guess!':
-                    self.guess_value = self.values['GuessValue']
-                    while self.try_again == True:
-                        if int(self.guess_value) > self.random_value:
-                            print('A little less!')
-                            break
-                        elif int(self.guess_value) < self.random_value:
-                            print('A little more!')
-                            break
-                        if int(self.guess_value) == self.random_value:
-                            self.try_again = False
-                            print('CONGRATULATIONS! You right!')
-                            break
-                if self.event == sg.WIN_CLOSED:
-                    break
-        except:
-            print('Please, insert only numbers!')
-            self.Start()
-            
+sg.theme('DarkAmber')   # Adiciona um tom de cor à janela.
+# Todas as coisas contidas na janela.
+dimensoesJanela = [  [sg.Text('Algum texto na linha 1')],
+            [sg.Text('Insira algo na linha 2'), sg.InputText(key='QualquerCoisa')],
+            [sg.Button('Ok'), sg.Button('Cancelar')],
+            [sg.Text(key='TextoQualquerCoisa')]
+              ]
 
-    def GenerateRandomNumber(self):
-        self.random_value =  random.randint(self.mininum_value, self.maximum_value)
+# Cria a janela (nome que fica no topo e as dimenções da janela)
+janela = sg.Window('Digitando Algo', dimensoesJanela)
+# Laço infinito (event lopp) para ativação da janela e recebimento dos eventos (cliques) e dos valores (entradas).
+while True:
+# "evento" armazena todos cliques nos botões e "valores" armazena as entradas, ambos feitos pelo usuário.
+    eventos, valores = janela.read()
+    # "WIN_CLOSED" é um evento padrão que corresponde a clicar no X no canto superior direito da janela.
+    if eventos == sg.WIN_CLOSED or eventos == 'Cancelar': # Caso o usuário feche a janela ou clique em cancelar.
+        break
+    # Se o usuário clicar no botão "Ok"
+    if eventos == 'Ok': 
+        entradas = valores['QualquerCoisa']
+        janela['TextoQualquerCoisa'].update(entradas)
 
-guess = GuessTheNumber()
-guess.Start()
+janela.close()
