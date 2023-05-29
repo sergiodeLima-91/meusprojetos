a67a5c728ee183f057c59ee1cbf8d76300d48348
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-29 14:46:06 -0300

    criacao-da-pagina-de-registro-dos-usuarios-captados-no-formulario

--- django/Registration_System/reg_sys_project/app_reg_sys/views.py
+++ django/Registration_System/reg_sys_project/app_reg_sys/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    # Salvar os dados da tela (colocadas no formulário) no banco de dados.
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    
+    # Exibir todos os usuários cadastrados numa nova página. Isso deve ser feito em forma de dicionário, pois é o que o Django estabelece como padrão. 
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    
+    # Retornar dados para a página de listagem de usuários
+    return render(request, 'usuarios/usuarios.html',usuarios)
